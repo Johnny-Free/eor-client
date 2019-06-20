a98b270112221cc481ecce1bae0b0e7b4933131c
Johnny Free <j.darell.free@gmail.com>
2019-06-20 07:43:53 -0500

    started contact page, will move alerts to modals, added google analytics

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Contact extends Component{
+    render(){
+        return(
+            <div id='parentConctact'>
+                <div id='contact'>
+                    
+                </div>
+            </div>
+        )
+    }
+}
