408ddce7342cc8a9ea61bdb772be77b046a35c87
Johnny Free <j.darell.free@gmail.com>
2019-06-20 07:46:26 -0500

    fixed typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,8 @@ export default class Header extends Component{
                     </p>
                     <p id='exampleBox'>
                         Example 1 Claim Number: WC11111 Tax Id: 123456789 Dos: 01/12/2019 Billed: 100.00<br/>
-                        Example 2 Claim Number: WC12345 Tax Id: 123789456 Dos: 02/17/2018 Billed: 650.00<br/>
-                        * Example 3 Claim Number: WC887554 Tax Id: 654987321 Dos: 01/01/2001 Billed: 450.00<br/>
+                        *Example 2 Claim Number: WC12345 Tax Id: 123789456 Dos: 02/17/2018 Billed: 650.00<br/>
+                        Example 3 Claim Number: WC887554 Tax Id: 654987321 Dos: 01/01/2001 Billed: 450.00<br/>
                         * Indicates inputs that will have multiple results ie recon
                     </p>
                 </div>
